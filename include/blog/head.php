0e5892895252db282b6fc744a1aeb695afc3b08b
Josh Green <josh@softwareverde.com>
2017-08-07 14:34:46 -0400

    Refactored directory structure for website compatibility.

--- include/blog/head.php
+++ include/blog/head.php
@@ -11,19 +11,19 @@
 <meta charset="utf-8"/>
 <meta name="viewport" content="width=device-width"/>
 <!-- CSS -->
-<link rel="stylesheet" type="text/css" href="/css/nerd-blog.css" />
+<link rel="stylesheet" type="text/css" href="/css/blog/blog.css" />
 <!-- Google Fonts -->
-<link href="https://fonts.googleapis.com/css?family=Roboto|Roboto+Mono" rel="stylesheet">
+<link href="//fonts.googleapis.com/css?family=Roboto|Roboto+Mono" rel="stylesheet">
 <!-- Font Awesome Icons -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 <!-- jQuery -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="//code.jquery.com/jquery-3.2.1.min.js"></script>
 <!-- JS -->
-<script src="/js/nerd-blog.js"></script>
+<script src="/js/blog/blog.js"></script>
 <?php
     if (isset($ADMIN)) {
-        ?><link rel="stylesheet" type="text/css" href="/css/nerd-blog-admin.css"/><?php
-        ?><script src="/js/nerd-blog-admin.js"></script><?php
+        ?><link rel="stylesheet" type="text/css" href="/css/blog/admin.css"/><?php
+        ?><script src="/js/blog/admin.js"></script><?php
     }
     if ($FAVICON) {
         ?><link rel="icon"          type="image/x-icon" href="/img/favicon.ico" /><?php
